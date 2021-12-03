a989440e474c4df017beb3c2d3624258942b92d9
Stan Hackman <shackma2@illinois.edu>
2021-12-03 14:25:26 -0600

    Fixed aspects of BFS.cc to work with list not queue. List has a native iterator

--- src/BFS.cc
+++ src/BFS.cc
@@ -1,5 +1,7 @@
 #include "BFS.h"
 #include<queue>
+#include<list>
+
 Graph::Graph(int vertex){
   this->vertex = vertex;//updates BFS vertex
   al = new list<int>[vertex];//creates a new adjacency list
@@ -11,18 +13,18 @@ void Graph::BFS(int s){//visits each vertex
   bool *wasVisited = new bool[vertex];//makes pointer to an array keeping track of whether or not the node has been checked
   for(int i = 0; i < vertex; i++)
     wasVisited[i] = false;//goes through each of the nodes and sets their visited value to unvisited
-  queue<int> Q;//creates a queue to keep track of the search
+ list<int> Q;//creates a queue to keep track of the search
   wasVisited[s] = true;//current node has been visited
   Q.push_back(s);//queue initial node
-  Q<int>::iterator it;//iterator to find adjacent vertecies
+  list<int>::iterator it;//iterator to find adjacent vertecies
   while(!Q.empty()){//search through until queue is empty
     s = Q.front();//set searcher to the front of  the queue
     cout << s << " ";//textual feedback
-    Q.pop();//remove from queue, it is being checked
+    Q.pop_front();//remove from queue, it is being checked
     for(it = al[s].begin(); it != al[s].end(); ++it){
       if(!wasVisited[*it]){
         wasVisited[*it] = true;//if it has not been visited, mark as visited and queue it up
-        Q.push_back(*it)
+        Q.push_back(*it);
       }
     }
   }
