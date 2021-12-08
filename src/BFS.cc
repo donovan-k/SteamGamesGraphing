93d27ae93a21f7dae53f765f006199f3014cbbd9
Stan Hackman <shackma2@illinois.edu>
2021-12-08 11:50:31 -0600

    updated BFS to use graph

--- src/BFS.cc
+++ src/BFS.cc
@@ -1,17 +1,15 @@
 #include "BFS.h"
-#include<queue>
-#include<list>
+#include "Graph.h"
+#include <list>
 
-BFSg::BFSg(int vertex){
-  this->vertex = vertex;//updates BFS vertex
-  al = new list<int>[vertex];//creates a new adjacency list
+BFSg::BFSg(Graph graphx){
+  al = graphx.getAdjacencyList();//sets pointer to graph
+  vert = graphx.size();//sets the size of the graph
 }
-void BFSg::edgeInstall(int v, int w){
-  al[v].push_back(w);//adds edge by adding something to the al
-}
+
 void BFSg::BFS(int s){//visits each vertex
-  bool *wasVisited = new bool[vertex];//makes pointer to an array keeping track of whether or not the node has been checked
-  for(int i = 0; i < vertex; i++)
+  bool *wasVisited = new bool[vert];//makes pointer to an array keeping track of whether or not the node has been checked
+  for(int i = 0; i < vert; i++)
     wasVisited[i] = false;//goes through each of the nodes and sets their visited value to unvisited
  list<int> Q;//creates a queue to keep track of the search
   wasVisited[s] = true;//current node has been visited
@@ -19,7 +17,7 @@ void BFSg::BFS(int s){//visits each vertex
   list<int>::iterator it;//iterator to find adjacent vertecies
   while(!Q.empty()){//search through until queue is empty
     s = Q.front();//set searcher to the front of  the queue
-    cout << s << " \n";//textual feedback
+    cout << s << " \n"; //textual feedback
     Q.pop_front();//remove from queue, it is being checked
     for(it = al[s].begin(); it != al[s].end(); ++it){
       if(!wasVisited[*it]){
