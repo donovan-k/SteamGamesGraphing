919e0c82c7a0873341b3786883b4d07cb27d7f85
Ganotisi, Pj <pganot2@illinois.edu>
2021-12-13 15:06:12 -0600

    Completed bfs.h
    No changes were made. Simply a final check for the final submission.

--- src/bfs.h
+++ src/bfs.h
@@ -5,6 +5,7 @@
 class BFSg {
   int vert;           // number of vertices
   std::list<int> *al; // points to adjacency lists
+  
 public:
   BFSg(Graph *graphx);                       // constructor
   std::vector<int> BFS(int index, int size); // prints BFS traversal
