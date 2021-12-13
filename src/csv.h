3025d103f6282784bb9b4e08073a06a0fe3156eb
Ganotisi, Pj <pganot2@illinois.edu>
2021-12-13 15:07:26 -0600

    Completed csv.h
    No changes were made. Simply a final check for the final submission.

--- src/csv.h
+++ src/csv.h
@@ -6,4 +6,5 @@
 void lex_token(std::deque<char> &buffer, std::deque<char> &output);
 void lex_string(std::deque<char> &buffer, std::deque<char> &output);
 void lex_escape(std::deque<char> &buffer, std::deque<char> &output);
+
 #endif // CSV_H
